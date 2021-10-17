b4652a6c7be4e706f3e2f9cb039f4bdad2f60720
OxPAN <kipir1@mail.ru>
2021-10-17 15:00:19 +0700

    Initial commit

---
+++ src/git/group/List/TList.java
@@ -0,0 +1,153 @@
+package git.group.List;
+
+public class TList<T>{
+    private class Node<T>{
+        public  Node<T> next;// указатель на следующий элемент
+        public T data;
+
+        public Node(){}
+        public Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node<T> head = null;//nachalo
+    public Node<T> tail = null;//endlist
+    public int size = 0;
+    public int size_limit = 20;
+
+    public boolean addNode(T data) {//vstavka v end
+        if (size < size_limit) {
+            Node<T> nNode = new Node<>(data);
+
+            if (head == null) {
+                head = nNode;
+                tail = nNode;
+            } else {
+                tail.next = nNode;//sleduuwii na tekuwii
+                tail = nNode;//tekuwii
+            }
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addNodeIndex(T data, int index){//dobav po index
+        if (size < size_limit) {
+            Node<T> nNode = new Node<>(data);
+
+            if (head == null) {
+                head = nNode;
+                tail = nNode;
+            } else {
+                Node<T> temp, current;
+                temp = head;
+                current = null;
+
+                for (int i = 0; i < index; i++) {
+                    current = temp;
+                    temp = temp.next;
+                }
+
+                current.next = nNode;
+                nNode.next = temp;
+            }
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean dropNodeIndex(int index){
+//        if (size <= 0)
+//            return false;
+
+        Node<T> temp,current;
+
+        if (head == null){
+            System.out.println("List is empty");
+            return false;
+        }else {
+            if (head != tail){
+                temp = head;
+                current = null;
+
+                for (int i = 0; i < index;i++){
+                    current = temp;
+                    temp = temp.next;
+                }
+                if (current != null){
+                    current.next = temp.next;
+                    temp = null;
+                }else {
+                    head = tail = temp.next;
+                    temp = null;
+                }
+            }else {
+                head = tail = null;
+            }
+        }
+        size--;
+        return true;
+    }
+
+    public T searchNodeIndex(int index){
+        T dataNode;
+        Node<T> current = head;
+
+        if (index == 0){
+            dataNode = current.data;
+            return dataNode;
+        }
+
+        for (int i = 0; i < index; i++){
+            current = current.next;
+        }
+
+        dataNode = current.data;
+
+        return dataNode;
+    }
+
+    public boolean showList(){
+        Node<T> current = head;
+
+        if (head == null){
+            System.out.println("List is empty");
+            return false;
+        }
+
+        while (current != null){
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.printf("");
+        return true;
+    }
+
+    public int countNodes(){//kolichestvo yzlov
+        int count = 0;
+        Node<T> current = head;
+
+        while (current != null){
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    ///zona getersSeters--------
+    public int getSize(){
+        return this.size;
+    }
+    public int getSize_limit(){return this.size_limit;}
+    public boolean setSize_limit(int limit){
+        if (limit <= 0 || limit <= size)
+            return false;
+
+        this.size_limit = limit;
+        return true;
+    }
+}
---
+++ src/git/group/Main.java
@@ -0,0 +1,35 @@
+package git.group;
+import git.group.List.TList;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("1");
+        TList<Integer> intlist = new TList<Integer>();
+        intlist.addNode(0);
+        intlist.addNode(1);
+        intlist.addNode(2);
+        intlist.addNode(3);
+        intlist.addNode(4);
+        System.out.println(intlist.addNode(9));
+        System.out.println(intlist.addNodeIndex(10,2));
+        System.out.println(intlist.addNodeIndex(10,1));
+        System.out.println(intlist.addNodeIndex(10,5));
+        intlist.showList();
+        System.out.printf("\nC: "+ intlist.countNodes());
+        System.out.println("\nSZ: "+ intlist.getSize());
+
+        intlist.dropNodeIndex(2);
+        intlist.dropNodeIndex(2);
+        intlist.dropNodeIndex(2);
+        intlist.dropNodeIndex(2);
+
+        intlist.showList();
+        System.out.printf("\nC: "+ intlist.countNodes());
+        System.out.println("\nSZ: "+ intlist.getSize());
+
+        System.out.println("dataind: " + intlist.searchNodeIndex(3));
+    }
+}
