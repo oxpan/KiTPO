fb2ff99f744fef6674f1282d70d360260bdd1faa
Aleksandr <johndoe@example.com>
2021-10-17 19:23:08 +0700

    Интерфейс Компаратора. Реализация для String. Интерфейс Builder

--- src/git/group/Main.java
+++ src/git/group/Main.java
@@ -1,35 +1,47 @@
 package git.group;
+import git.group.Comparator.Comparator;
+import git.group.Comparator.ComparatorString;
 import git.group.List.TList;
 
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-//        System.out.println("1");
-        TList<Integer> intlist = new TList<Integer>();
-        intlist.addNode(0);
-        intlist.addNode(1);
-        intlist.addNode(2);
-        intlist.addNode(3);
-        intlist.addNode(4);
-        System.out.println(intlist.addNode(9));
-        System.out.println(intlist.addNodeIndex(10,2));
-        System.out.println(intlist.addNodeIndex(10,1));
-        System.out.println(intlist.addNodeIndex(10,5));
-        intlist.showList();
-        System.out.printf("\nC: "+ intlist.countNodes());
-        System.out.println("\nSZ: "+ intlist.getSize());
+//	// write your code here
+//    //  System.out.println("1");
+//        TList<Integer> intlist = new TList<Integer>();
+//        intlist.addNode(0);
+//        intlist.addNode(1);
+//        intlist.addNode(2);
+//        intlist.addNode(3);
+//        intlist.addNode(4);
+//        System.out.println(intlist.addNode(9));
+//        System.out.println(intlist.addNodeIndex(10,2));
+//        System.out.println(intlist.addNodeIndex(10,1));
+//        System.out.println(intlist.addNodeIndex(10,5));
+//        intlist.showList();
+//        System.out.printf("\nC: "+ intlist.countNodes());
+//        System.out.println("\nSZ: "+ intlist.getSize());
+//
+//        intlist.dropNodeIndex(2);
+//        intlist.dropNodeIndex(2);
+//        intlist.dropNodeIndex(2);
+//        intlist.dropNodeIndex(2);
+//
+//        intlist.showList();
+//        System.out.printf("\nC: "+ intlist.countNodes());
+//        System.out.println("\nSZ: "+ intlist.getSize());
+//
+//        System.out.println("dataind: " + intlist.searchNodeIndex(3));
 
-        intlist.dropNodeIndex(2);
-        intlist.dropNodeIndex(2);
-        intlist.dropNodeIndex(2);
-        intlist.dropNodeIndex(2);
 
-        intlist.showList();
-        System.out.printf("\nC: "+ intlist.countNodes());
-        System.out.println("\nSZ: "+ intlist.getSize());
+        Comparator comparator = new ComparatorString();
 
-        System.out.println("dataind: " + intlist.searchNodeIndex(3));
+        String str1 = "abrft";
+        String str2 = "cdeqw";
+
+        int res = comparator.compare(str1,str2);
+
+        System.out.println(res);
     }
 }
